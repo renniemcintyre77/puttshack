26c9edd82070c33175e7b10bbdc3099eee8af8ca
Rennie McIntyre <rennie.mcintyre@sky.uk>
2021-10-21 10:47:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{backgroundImage: `url('puttshack.png')`, width: '100vw', height: '100vh', backgroundPosition: 'center' }}>
+      
+    </div>
+  );
+}
+
+export default App;
